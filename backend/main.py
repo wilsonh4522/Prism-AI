5a57ec2fca15bee14d70dc0d954d94916524d5d6
Wilson Huang <wilsonh2716@gmail.com>
2026-05-28 00:04:36 -0400

    add polygon AI integration

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+import httpx
+
+load_dotenv()
+
+POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
 
 app = FastAPI()
 
@@ -13,3 +20,12 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "Prism API is running"}
+
+@app.get("/analyze/{ticker}")
+def get_ticker(ticker):
+    response = httpx.get(f"https://api.polygon.io/v3/reference/tickers/{ticker}?apiKey={POLYGON_API_KEY}")
+    data = httpx.get(f"https://api.polygon.io/v2/aggs/ticker/{ticker}/prev?apiKey={POLYGON_API_KEY}")
+    return {
+        "details": response.json(),
+        "price": data.json()
+    }
